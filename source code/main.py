4c86a8d0a25511b14ce8609c5febc500bea5ab92
Gusic06 <97785573+Gusic06@users.noreply.github.com>
2022-11-06 19:32:08 +0000

    Update main.py

--- source code/main.py
+++ source code/main.py
@@ -5,6 +5,31 @@ from shutdown import shutdown
 import time
 import os
 
+###################################################################
+
 installer("https://github.com/Gusic06/computer-outro-small-refactor-/blob/main/source%20code/bsod.py", 
-          "https://github.com/Gusic06/computer-outro-small-refactor-/blob/main/source%20code/fakebsod.py", 
-          "https://github.com/Gusic06/computer-outro-small-refactor-/blob/main/source%20code/shutdown.py")
+    "https://github.com/Gusic06/computer-outro-small-refactor-/blob/main/source%20code/fakebsod.py", 
+    "https://github.com/Gusic06/computer-outro-small-refactor-/blob/main/source%20code/shutdown.py")
+
+###################################################################
+
+fakeBsodInit = """import PyInstaller.__main__
+
+PyInstaller.__main__.run([
+    "fakebsod.py",
+    "--onefile"
+])"""
+    
+exec(fakeBsodInit)
+
+###################################################################
+
+shutdownInit = """import PyInstaller.__main__
+    
+PyInstaller.__main__.run([
+    "shutdown.py",
+    "--onefile"
+])
+"""
+    
+exec(shutdownInit)
