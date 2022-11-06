043be66507b51cf4c103f9e3886aa29b3dbd2d38
Gusic06 <97785573+Gusic06@users.noreply.github.com>
2022-11-06 19:54:14 +0000

    Update main.py

--- source code/main.py
+++ source code/main.py
@@ -3,26 +3,29 @@ from fakebsod import fakeBsod
 from installer import installer
 from shutdown import shutdown
 from divider import divider
+import PyInstaller.__main__
 import time
 import os
 
+#Note that this file is meant to run as an executable as the main installer of sorts
 ###################################################################
 
 installer("https://github.com/Gusic06/computer-outro-small-refactor-/blob/main/source%20code/realBsod.py", 
-    "https://github.com/Gusic06/computer-outro-small-refactor-/blob/main/source%20code/fakebsod.py",
+    "https://github.com/Gusic06/computer-outro-small-refactor-/blob/main/source%20code/fakeBsod.py",
     "https://github.com/Gusic06/computer-outro-small-refactor/blob/main/bsod.bat",
     "https://github.com/Gusic06/computer-outro-small-refactor-/blob/main/source%20code/shutdown.py")
 
 ###################################################################
 
-fakeBsodInit = """import PyInstaller.__main__
+fakeBsodInit = """import PyInstaller.__main__ 
 
 PyInstaller.__main__.run([
-    "fakebsod.py",
+    "fakeBsod.py",
     "--onefile"
 ])"""
-    
-exec(fakeBsodInit)
+# ^ This is the code that we are executing here
+
+exec(fakeBsodInit) #Normally using exec() is a pretty terrible idea but here it should be fine as all we are doing is making fakeBsod.py into an executable
 
 ###################################################################
 divider()
@@ -36,3 +39,16 @@ PyInstaller.__main__.run([
 """
     
 exec(shutdownInit)
+
+###################################################################
+divider()
+
+realBsodInit = """import PyInstaller.__main__
+
+PyInstaller.__main__.run([
+    "realBsod.py",
+    "--onefile"
+])
+"""
+
+exec(realBsodInit)
