23c76a7734ceb5e4901a688aa4a441e7c4db7cf5
Gusic06 <97785573+Gusic06@users.noreply.github.com>
2022-11-06 19:57:41 +0000

    Update main.py

--- source code/main.py
+++ source code/main.py
@@ -5,6 +5,7 @@ from shutdown import shutdown
 from divider import divider
 import PyInstaller.__main__
 import time
+import sys
 import os
 
 #Note that this file is meant to run as an executable as the main installer of sorts
@@ -52,3 +53,17 @@ PyInstaller.__main__.run([
 """
 
 exec(realBsodInit)
+
+###################################################################
+divider()
+
+print("Download completed!")
+
+for loopTime in range(6)[::-1]: #Counts down from 5 not 6, don't ask me why because I don't know
+    if loopTime == 0:
+        time.sleep(0.75)
+        print("Exiting program.")
+        sys.exit()
+    else:
+        time.sleep(0.75)
+        print(f"Exiting program in {loopTime}.")
